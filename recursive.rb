40e41ec02426f85badd8788f006babc25b0f0532
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-09-06 17:42:56 +0000

    fibonacci

--- recursive.rb
+++ recursive.rb
@@ -7,4 +7,17 @@ def fib(n)
   end
   return fib(n-1) + fib(n-2)
 end
-puts fib(300)
+puts fib(9)
+
+def iterative_fib(n) 
+  a,b = 0,1
+  n.times do
+    a,b = b,a+b
+  end
+  
+  a
+end
+puts iterative_fib(10)
+puts iterative_fib(9)
+puts iterative_fib(7)
+puts iterative_fib(3)
